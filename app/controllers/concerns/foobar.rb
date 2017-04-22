3d0ab0c63b620ac4e5021f51088ca280ff6619d1
Raffael Nagel <raffael.nagel@gmail.com>
2017-04-22 10:14:14 -0700

    first commit

---
+++ app/controllers/concerns/foobar.rb
@@ -0,0 +1,27 @@
+class Foobar
+  def initialize(arg)
+  	@middle=arg
+  end
+  def bar(cat, hsh)
+  	return cat.to_s+@middle.to_s+hsh[:sat].to_s
+  end
+end
+
+class Person
+	def initialize(name,age)
+		@name=name
+		@age=age
+		@nickname=name.to_s[0..4]
+	end
+	def introduce
+		return @name.to_s+' '+@age.to_s
+	end
+	def birth_year
+		return Time.now.year.to_i-@age.to_i
+	end
+
+	def nickname
+		return @nickname
+	end
+
+end
